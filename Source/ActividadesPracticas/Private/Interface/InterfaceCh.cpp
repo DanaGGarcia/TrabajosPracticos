845dae23c0c96de1f4933a5642a672583dcb5e29
Dana Garcia <141261839+danagg12@users.noreply.github.com>
2025-09-06 17:43:21 -0300

    Trabajo Grupal

--- Source/ActividadesPracticas/Private/Interface/InterfaceCh.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Interface/InterfaceCh.h"
-
-// Add default functionality here for any IInterfaceCh functions that are not pure virtual.
